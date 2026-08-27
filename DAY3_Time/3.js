9ace02c2a7cd1a30387bcebe3532f3242f006d74
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-08-27 09:54:30 +0530

    Start DSA 3Q per day

--- DAY3_Time/3.js
+++
@@ -1,9 +0,0 @@
-function bulidarray(nums) {
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    (ans[i] = nums[nums[i]]);
-  }
-    return ans;
-}
-
-console.log(bulidarray([0,2,1,5,3,4]));
