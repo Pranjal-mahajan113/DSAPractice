10caf2df753d22fad1083b978979266b4851210e
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-04 16:46:58 +0530

    Practice question

---
+++ September/PracticeDay1.js
@@ -0,0 +1,10 @@
+//Q2
+function singleNumber(nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result = result ^ nums[i];
+  }
+  return result;
+}
+
+console.log(singleNumber([1,2,2,1,4,4,5]))
