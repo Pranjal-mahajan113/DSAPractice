142185ceb58fa67230871db6be6b9357ddd46e3c
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-05 20:59:59 +0530

    Fix Day 5 Revision file

--- September/DAY5RevisionDay.js
+++ September/DAY5RevisionDay.js
@@ -18,4 +18,77 @@ function removeVal(nums, val) {
   }
   return x;
 }
-console.log(removeVal([3, 2, 2, 3], 3));
+// console.log(removeVal([3, 2, 2, 3,1], 3));
+
+// Q3
+function reverseString(s) {
+  let left = 0;
+  let right = s.length - 1; //5-1=4 4th position of array is ->o
+  while (left < right) {
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+  return s;
+}
+// console.log(reverseString(["h", "e", "l", "l", "0"]));
+
+//Q4
+function moveZeroEnd(nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[x] = nums[i];
+      x++;
+    }
+  }
+  while (x < nums.length) {
+    nums[x] = 0;
+    x++;
+  }
+}
+let nums = [0, 1, 0, 3, 12];
+moveZeroEnd(nums);
+// console.log(nums);
+
+//Q6
+function maximumConsecive(nums) {
+  let count = 0;
+  let maxcount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+      maxcount = count;
+    } else {
+      count = 0;
+    }
+  }
+  return maxcount;
+}
+// console.log(maximumConsecive([1, 1, 0, 1, 1, 1]));
+
+// Q7
+function missingNumber(nums) {
+  let n = nums.length;
+  let actualnum = 0;
+  let expectedSum = (n * (n + 1)) / 2;
+  for (let i = 0; i < nums.length; i++) {
+    actualnum += nums[i];
+  }
+  return expectedSum - actualnum;
+}
+console.log(missingNumber([3, 0, 1]));
+
+//Q8
+function singleNumber(nums){
+    let result=0;
+    for(let i=0;i<nums.length;i++){
+        result= result^nums[i]
+    }
+    return result;
+}
+console.log(singleNumber([4,1,2,1,2]))
+
+//9->Merge sorted array
