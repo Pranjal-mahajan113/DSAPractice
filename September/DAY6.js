e579adfaf99f371577de9cd0ab1781d853f00c7a
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-07 20:23:48 +0530

    Merge Sort

--- September/DAY6.js
+++ September/DAY6.js
@@ -1,8 +1,30 @@
-function targetReturn(nums,target){
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            return i;
-        }
+function targetReturn(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
     }
+  }
 }
-console.log(targetReturn([10,25,7,40,15],40))
+console.log(targetReturn([10, 25, 7, 40, 15], 40));
+
+function targetShirt(nums, target) {
+  let low = 0;
+  let high = nums.length-1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+}
+console.log(targetShirt([-1, 0, 3, 5, 9, 12], 9));
+
+
+function TargetedequalTo(nums){
+    let 
+}
