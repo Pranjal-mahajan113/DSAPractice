cc41b4986066467cf65e565a57977bba55d64017
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-08 18:29:07 +0530

    Recursion fibonacci

--- September/DAY8.js
+++ September/DAY8.js
@@ -3,4 +3,20 @@ function fun(n) {
   let ans = fun(n - 1);
   return ans + n;
 }
-console.log(fun(4));
+// console.log(fun(4));
+
+
+var fib = function(n) {
+    
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n === 1) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+
+};
+console.log(fib(3))
