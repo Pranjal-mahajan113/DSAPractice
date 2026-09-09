5130cfa0c60dcb349ead30863588be220af70fac
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-09 18:15:57 +0530

    Recrusion Practice questions

--- September/DAY9.js
+++ September/DAY9.js
@@ -1,39 +1,56 @@
-function findmax(arr, n) {
-  if (n === 1) {
-    return arr[0];
-  }
-  let last = arr[n - 1];
+// function findmax(arr, n) {
+//   if (n === 1) {
+//     return arr[0];
+//   }
+//   let last = arr[n - 1];
 
-  let answer = findmax(arr, n - 1);
-  if (last > answer) {
-    return last;
-  } else {
-    return answer;
-  }
-}
-// let arr = [4, 8, 2, 10, 6];
+//   let answer = findmax(arr, n - 1);
+//   if (last > answer) {
+//     return last;
+//   } else {
+//     return answer;
+//   }
+// }
+// // let arr = [4, 8, 2, 10, 6];
 
-// console.log(findmax(arr, arr.length));
+// // console.log(findmax(arr, arr.length));
 
-function sum(arr, n) {
-  if (n === 1) {
-    return arr[0];
-  }
-  let last = arr[n - 1];
-  let answer = sum(arr, n - 1);
-  return last + answer;
-}
+// function sum(arr, n) {
+//   if (n === 1) {
+//     return arr[0];
+//   }
+//   let last = arr[n - 1];
+//   let answer = sum(arr, n - 1);
+//   return last + answer;
+// }
 
-let arr = [3, 5, 2, 7, 4];
-console.log(sum(arr,arr.length))
+// let arr = [3, 5, 2, 7, 4];
+// console.log(sum(arr,arr.length))
 
 
-function productofArray(arr,n){
+// function productofArray(arr,n){
+//     if(n===1){
+//         return arr[0];
+//     }
+//     let last = arr[n-1];
+//     let answer=productofArray(arr,n-1)
+//     return last * answer;
+// }
+// console.log(product([2, 3, 4, 5], 4));
+
+
+function maxNumber(arr,n){
     if(n===1){
-        return arr[0];
+        return arr[0]
     }
-    let last = arr[n-1];
-    let answer=productofArray(arr,n-1)
-    return last * answer;
+    let last=arr[n-1];
+    let answer=maxNumber(arr,n-1)
+    if(last>answer){
+        return last;
+    }
+    else{
+        return answer;
+    }
 }
-console.log(product([2, 3, 4, 5], 4));
+let arr=[5, 12, 7, 20, 9];
+console.log(maxNumber(arr,arr.length))
