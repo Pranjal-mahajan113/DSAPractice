b4e5d87dcdb22dc4f048fd23c3e3801973637116
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-09-09 17:07:00 +0530

    FindMax recursion

--- September/DAY9.js
+++ September/DAY9.js
@@ -1,6 +1,18 @@
-function findmax(arr,n){
- if(n===1){
+function findmax(arr, n) {
+  if (n === 1) {
     return arr[0];
- }   
- 
+  }
+  let last = arr[n - 1];
+
+  let answer = findmax(arr, n - 1);
+  if (last > answer) {
+    return last;
+  } else {
+    return answer;
+  }
 }
+let arr = [4, 8, 2, 10, 6];
+
+console.log(findmax(arr, arr.length));
+
+
